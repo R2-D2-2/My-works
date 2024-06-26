b9574cee90207aae42bf4ed8cd247db79a735f10
Andrey <123240200+R2-D2-2@users.noreply.github.com>
2024-06-26 23:45:58 +0400

    Add files via upload

---
+++ New folder 06.24/hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void)
+{
+    char byte_memory; //unsigned - беззнаковый, signed - знаковый
+    byte_memory = 100;
+    char a;
+    a = byte_memory;
+    /*printf("Hello\n");
+    return 0;*/
+}
